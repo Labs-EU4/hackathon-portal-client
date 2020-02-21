31cd04335ac564a08e02843b50561dfcf12958ec
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2020-02-21 10:52:21 +0100

    Adding context to test readability of Netlify variables

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import ResetPasswordConfirmation from './components/views/resetPassword/ResetPas
 import NewPassword from './components/views/resetPassword/NewPassword';
 
 function App() {
-  console.log(process.env)
+  console.log(process.env.CONTEXT)
   return (
     <>
       <GlobalStyles />
