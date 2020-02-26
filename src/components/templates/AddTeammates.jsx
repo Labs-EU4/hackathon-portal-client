351e3469de4ccff989e5e69c148625a8541258e4
AbdelI <homeworkoracle@protonmail.com>
2020-02-26 11:41:16 +0100

    Tests on AddTeammates all passing

--- src/components/templates/AddTeammates.jsx
+++ src/components/templates/AddTeammates.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import isEmail from 'validator/lib/isEmail';
+import isEmail from "validator/lib/isEmail";
 import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { Footer } from "../organisms/index";
@@ -7,27 +7,25 @@ import UserHeader from "../organisms/UserHeader";
 import WideBody from "../atoms/WideBody";
 import { H3 } from "../atoms/Heading";
 import { RowHead } from "../atoms/RowHead";
-// import { RowBody } from "../atoms/RowBody";
 import { Column } from "../atoms/Column";
 import { CardWide } from "../atoms/Card";
-// import Button from "../atoms/Button";
 import { addTeamMember, sendEventTeamInvite } from "../../store/events/actions";
-import { useSearchUserByEmail } from '../../hooks';
+import { useSearchUserByEmail } from "../../hooks";
 import Nav from "../molecules/Nav";
 import { BodyContainerColumn } from "../styles/templates/AddTeammatesStyling";
 import { SearchWidget, RoleWidget, InviteWidget } from "./widgets";
 
 const AddTeammates = () => {
   const [selectedUser, setSelectedUser] = useState(null);
-  const [role, setRole] = useState("");
+  const [role] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
   const { id } = useParams();
   const [matches, searchString, setSearchString] = useSearchUserByEmail();
   const [noneUser, setNoneUser] = useState(null);
-  const validateEmail = (email) => {
+  const validateEmail = email => {
     return isEmail(email);
-  }
+  };
 
   const handleSubmit = () => {
     const { email } = selectedUser;
@@ -45,8 +43,8 @@ const AddTeammates = () => {
       email: noneUser,
       role
     };
-    dispatch(sendEventTeamInvite(data, history))
-  }
+    dispatch(sendEventTeamInvite(data, history));
+  };
 
   return (
     <div>
@@ -59,9 +57,24 @@ const AddTeammates = () => {
           </RowHead>
           <Column>
             <CardWide>
-              {!selectedUser ? <SearchWidget setSelectedUser={setSelectedUser} setNoneUser={setNoneUser} matches={matches} searchString={searchString} setSearchString={setSearchString} validateEmail={validateEmail} />
-                : <RoleWidget selectedUser={selectedUser} handleSubmit={handleSubmit} />}
-              {noneUser ? <InviteWidget noneUser={noneUser} sendInvite={sendInvite} /> : null}
+              {!selectedUser ? (
+                <SearchWidget
+                  setSelectedUser={setSelectedUser}
+                  setNoneUser={setNoneUser}
+                  matches={matches}
+                  searchString={searchString}
+                  setSearchString={setSearchString}
+                  validateEmail={validateEmail}
+                />
+              ) : (
+                <RoleWidget
+                  selectedUser={selectedUser}
+                  handleSubmit={handleSubmit}
+                />
+              )}
+              {noneUser ? (
+                <InviteWidget noneUser={noneUser} sendInvite={sendInvite} />
+              ) : null}
             </CardWide>
           </Column>
         </BodyContainerColumn>
