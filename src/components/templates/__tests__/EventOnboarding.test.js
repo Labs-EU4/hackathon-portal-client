6a2681284640e83a6f78776746a81fc077454da6
AbdelI <homeworkoracle@protonmail.com>
2020-02-26 17:35:02 +0100

    DRY code for EventOnBoarding.test.js, using Data from the mock file utils

--- src/components/templates/__tests__/EventOnboarding.test.js
+++ src/components/templates/__tests__/EventOnboarding.test.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import "@testing-library/jest-dom/extend-expect";
+import { initialState } from "../../../utils/mockData";
+
 import EventOnboarding from "../EventOnboarding";
 
 const history = createMemoryHistory();
@@ -16,47 +18,6 @@ let mockStore;
 let store;
 
 beforeEach(() => {
-  const initialState = {
-    events: {
-      data: [
-        {
-          id: 1,
-          event_title: "EuroHack",
-          event_description: "the best event hack hack hack macbook",
-          creator_id: 1,
-          start_date: "10/09/11",
-          end_date: "08/07/13",
-          location: "Europe",
-          guidelines: "cool hekhkdsjfhsdkjf skdjfhskdjfhkjsdfh skjdfhksjdfh",
-          participation_type: "Team",
-          category_id: 1
-        },
-        {
-          id: 2,
-          event_title: "World",
-          event_description: "number one hacker event in the world",
-          creator_id: 1,
-          start_date: "07/09/11",
-          end_date: "08/07/13",
-          location: "Asia",
-          guidelines: "come well prepared,no cheating,no fighting",
-          participation_type: "Team",
-          category_id: 2
-        }
-      ]
-    },
-
-    currentUser: {
-      token: 347534574536,
-      userId: 1,
-      fullname: "Jake Tom",
-      email: "somemail@google.com",
-      username: "Jake22",
-      bio: "",
-      image: "",
-      image_url: {}
-    }
-  };
   mockStore = configureStore();
   // let wrapper;
   store = mockStore(initialState);
