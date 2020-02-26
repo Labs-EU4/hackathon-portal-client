df7f6aaffe653de48a8e7d0b997c17c6eb5ffaf6
AbdelI <homeworkoracle@protonmail.com>
2020-02-26 17:33:57 +0100

    DRY code for HackathonSingle.test.js, using Data from the mock file utils

--- src/components/templates/__tests__/HackathonSingle.test.js
+++ src/components/templates/__tests__/HackathonSingle.test.js
@@ -6,6 +6,7 @@ import { render, cleanup } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import "@testing-library/jest-dom/extend-expect";
 import HackathonSingle from "../HackathonSingle";
+import { initialState } from "../../../utils/mockData";
 
 const history = createMemoryHistory();
 
@@ -16,54 +17,6 @@ let mockStore;
 let store;
 
 beforeEach(() => {
-  const initialState = {
-    events: {
-      data: [
-        {
-          id: 1,
-          event_title: "EurooHack",
-          event_description: "the best event hack hack hack macbook",
-          creator_id: 1,
-          start_date: "10/09/11",
-          end_date: "08/07/13",
-          location: "Europe",
-          guidelines: "cool hekhkdsjfhsdkjf skdjfhskdjfhkjsdfh skjdfhksjdfh",
-          participation_type: "Team",
-          category_id: 1
-        },
-        {
-          id: 2,
-          event_title: "Woorld",
-          event_description: "number one hacker event in the world",
-          creator_id: 2,
-          start_date: "07/09/11",
-          end_date: "08/07/13",
-          location: "Asia",
-          guidelines: "come well prepared,no cheating,no fighting",
-          participation_type: "Team",
-          category_id: 2
-        }
-      ],
-      categories: [
-        { id: 2, category_name: "Summer Hackton" },
-        { id: 3, category_name: "World Hackton" },
-        { id: 3, category_name: "Asia Hackton" },
-        { id: 3, category_name: "Gamer's Hackton" },
-        { id: 3, category_name: "Advanced Hackton" }
-      ]
-    },
-
-    currentUser: {
-      token: 347534574536,
-      userId: 1,
-      fullname: "Jake Tom",
-      email: "somemail@google.com",
-      username: "Jake22",
-      bio: "",
-      image: "",
-      image_url: {}
-    }
-  };
   mockStore = configureStore();
   // let wrapper;
   store = mockStore(initialState);
