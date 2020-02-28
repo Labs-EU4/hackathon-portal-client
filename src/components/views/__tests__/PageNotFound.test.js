b38a0a46d2761d0072ef149ec7668d965ccdd250
AbdelI <homeworkoracle@protonmail.com>
2020-02-28 17:56:21 +0100

    Test for the 404 error message, passing

--- src/components/views/__tests__/PageNotFound.test.js
+++ src/components/views/__tests__/PageNotFound.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router,Route } from "react-router";
+import { Router, Route } from "react-router";
 import { createMemoryHistory } from "history";
 import { Provider } from "react-redux";
 import { render, cleanup } from "@testing-library/react";
@@ -30,22 +30,22 @@ beforeEach(() => {
 });
 
 describe("Shows all the text nodes on PageNotFound.js", () => {
-    // it("Should display the h3 title", () => {
-    //   expect(jestFeatures.getByText("My hackathons")).not.toBeDisabled();
-    // });
+  // it("Should display the h3 title", () => {
+  //   expect(jestFeatures.getByText("My hackathons")).not.toBeDisabled();
+  // });
   //   it("should be displaying the button text node,to create a new event ", () => {
   //     let form = () => jestFeatures.getByText("Create New");
 
   //     expect(form()).toBeInTheDocument();
   //   });
 
-    it("displays the img element shown for the 404 error ", () => {
-      expect(jestFeatures.getByAltText("404 icon")).toBeInTheDocument();
-    });
+  it("displays the img element shown for the 404 error ", () => {
+    expect(jestFeatures.getByAltText("404 icon")).toBeInTheDocument();
+  });
 
-  //   it("displays a part of the description from an already created event", () => {
-  //     expect(jestFeatures.getByText(/macbook/)).toBeInTheDocument();
-  //   });
+  it("displays a part of the description from an already created event", () => {
+    expect(jestFeatures.getByText("Error code: 404")).toBeInTheDocument();
+  });
   //   it("should not be displaying the 'you have not created events phrase,since two events are already listed' ", () => {
   //     let sentence = () =>
   //       jestFeatures.queryByText(/You have not created any events yet/);
