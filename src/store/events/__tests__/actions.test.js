e6e62f25d9357a8510e3445ecfbc71fd3381a1d2
AbdelI <homeworkoracle@protonmail.com>
2020-03-01 14:25:41 +0100

    Test for the  fcreateEvent  action creator, passing

--- src/store/events/__tests__/actions.test.js
+++ src/store/events/__tests__/actions.test.js
@@ -2,7 +2,7 @@ import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import * as actions from "../actions";
 import * as types from "../actions";
-import events from "../../../utils/mockData";
+import { initialState } from "../../../utils/mockData";
 
 afterEach(cleanup);
 
@@ -13,5 +13,23 @@ describe("Ensures that the action creators functions are working properly", () =
     };
 
     expect(actions.fetchAllEvents()).toEqual(expectedAction);
+  });
+  it("should create an action to fetch all events", () => {
+    const expectedAction = {
+      type: types.EventsTypes.FETCH_ALL_EVENTS
+    };
+
+    expect(actions.fetchAllEvents()).toEqual(expectedAction);
+  });
+  it("should create an action for createEvent", () => {
+    const expectedAction = {
+      payload: initialState.events.data[1],
+      history: "/dashboard/new",
+      type: types.EventsTypes.CREATE_EVENT
+    };
+
+    expect(
+      actions.createEvent(initialState.events.data[1], "/dashboard/new")
+    ).toEqual(expectedAction);
   });
 });
