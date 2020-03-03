3e54568a5c6b24e6f8dfcbb2c7ab4e374b9708b0
AbdelI <homeworkoracle@protonmail.com>
2020-03-03 22:04:56 +0100

    Test for component rendering, with private routes, passing

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import "@testing-library/jest-dom/extend-expect";
+import { initialState } from "../utils/mockData";
+import App from "../App";
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+const history = createMemoryHistory();
 
-const renderWithRouter = Component => render(
-  <Router history={history}>
-    <Route component={Component} />
-  </Router>
-)
+let component;
+let mockStore;
+let store;
 
-describe("It should render Application components correctly", () => {
-  
-    it("renders App component correctly", () => {
-      const render = () => renderWithRouter(App)
-      // const { getByText } = render(render);
-      // const formTitle = getByText(/Create an account/i);
-      expect(render).toMatchSnapshot();
-      // expect(formTitle).toBeInTheDocument();
-    });
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    id: 1
+  }),
+  useRouteMatch: () => ({ url: "/dashboard/event/:id/participant_submission" })
+}));
+
+beforeEach(() => {
+  mockStore = configureStore();
+  store = mockStore(initialState);
+  component = render(
+    <Router history={history}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  );
+});
+
+describe("Shows all the text nodes on CreateTeam.js that are contained on the making an event form", () => {
+  it("App.js component renders properly,with all the Private Routes", () => {
+    expect(component).toMatchSnapshot();
   });
+  // it("The text node for the first character of the current user mail, renders properly on the menu", () => {
+  //   expect(component.queryByText(/8/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the <h3> Participant Teams text node, renders properly", () => {
+  //   expect(component.queryByText(/Participant Teams/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the <h4> You are creating a team for text node, renders properly", () => {
+  //   expect(
+  //     component.queryByText(/You are creating a team for/i)
+  //   ).toBeInTheDocument();
+  // });
+  // it("The text node for the team being created for the selected event 'EuroHack' <span>, renders properly", () => {
+  //   expect(component.queryByText(/EuroHack/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the Team Name <label> input, renders properly", () => {
+  //   expect(component.queryByText(/Team Name/i)).toBeInTheDocument();
+  // });
+  // it("The role for the button used to submit the form to create a team renders properly", () => {
+  //   expect(component.getByRole(/button/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the submit button <button>, renders properly", () => {
+  //   expect(component.getByText(/submit/i)).toBeInTheDocument();
+  // });
+});
