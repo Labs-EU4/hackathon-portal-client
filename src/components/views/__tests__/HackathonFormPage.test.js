631d7949869bf7b3306e381ea5951336e9701c6e
AbdelI <homeworkoracle@protonmail.com>
2020-03-03 12:02:53 +0100

    Test for irst character of the user's email shows up on the menu, passing

--- src/components/views/__tests__/HackathonFormPage.test.js
+++ src/components/views/__tests__/HackathonFormPage.test.js
@@ -6,6 +6,7 @@ import { render } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import "@testing-library/jest-dom/extend-expect";
 import { initialState } from "../../../utils/mockData";
+import { screen } from "@testing-library/dom";
 import HackathonFormPage from "../HackathonFormPage";
 
 const history = createMemoryHistory();
@@ -31,11 +32,7 @@ describe("Component HackathonProjectsPage.js renders properly", () => {
   it("asserts that the component renders properly", () => {
     expect(component).toMatchSnapshot();
   });
-
-  // it("asserts that the Title of the event text node renders properly ", () => {
-  //   expect(component.queryByText(/World/i)).toBeInTheDocument();
-  // });
-  // it("asserts that the <img> logo for the FB icon renders properly ", () => {
-  //   expect(component.queryByAltText(/facebook/i)).toBeInTheDocument();
-  // });
+  it("asserts that the first character of the user's email shows up on the menu", () => {
+    expect(component.getByText("8")).toBeInTheDocument();
+  });
 });
