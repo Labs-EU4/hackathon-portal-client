afbd85e1c301d2771220b5a6a39a7984ec5d04b1
AbdelI <homeworkoracle@protonmail.com>
2020-03-03 11:37:37 +0100

    Removed the previous tests as they were failing

--- src/components/views/__tests__/HackathonFormPage.test.js
+++ src/components/views/__tests__/HackathonFormPage.test.js
@@ -1,20 +1,49 @@
-import React from 'react'
-import { Router, Route } from 'react-router';
-import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react'
-import HackathonFormPage from '../HackathonFormPage';
-const history = createMemoryHistory()
+import React from "react";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import "@testing-library/jest-dom/extend-expect";
+import { initialState } from "../../../utils/mockData";
+import HackathonFormPage from "../HackathonFormPage";
 
-const renderWithRouter = Component => render(
-  <Router history={history}>
-    <Route component={Component} />
-  </Router>
-)
+const history = createMemoryHistory();
 
-describe("It should render HackathonSinglePage page correctly", () => {
-  
-    it("renders the HackathonSinglePage component correctly", () => {
-      const render = () => renderWithRouter(HackathonFormPage)
-      expect(render).toMatchSnapshot();
-    });
+let mockStore;
+let store;
+let component;
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    id: 2
+  }),
+  useRouteMatch: () => ({ url: "/dashboard/event/:id" })
+}));
+
+beforeEach(() => {
+  mockStore = configureStore();
+  store = mockStore(initialState);
+
+  component = render(
+    <Router history={history}>
+      <Provider store={store}>
+        <HackathonFormPage />
+      </Provider>
+    </Router>
+  );
+});
+
+describe("Component HackathonProjectsPage.js renders properly", () => {
+  it("asserts that the component renders properly", () => {
+    expect(component).toMatchSnapshot();
   });
+
+  // it("asserts that the Title of the event text node renders properly ", () => {
+  //   expect(component.queryByText(/World/i)).toBeInTheDocument();
+  // });
+  // it("asserts that the <img> logo for the FB icon renders properly ", () => {
+  //   expect(component.queryByAltText(/facebook/i)).toBeInTheDocument();
+  // });
+});
