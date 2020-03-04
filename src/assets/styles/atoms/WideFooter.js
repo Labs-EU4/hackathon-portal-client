7431d7a02c94abbdd1d0e767af0953fed96396a8
Karim B <mattiakarim@live.it>
2020-03-04 18:24:39 +0100

    Refactor WideBody, WideFooter and move them to the assets atoms folder, added TextArea file

--- src/assets/styles/atoms/WideFooter.js
+++ src/assets/styles/atoms/WideFooter.js
@@ -1,11 +1,9 @@
 import styled from "styled-components";
-import { media, Solid } from '../index';
+import { media } from '../../assets/styles/variables/media';
 
 const WideFooter = styled.div`
   display: flex;
   justify-content: center;
-  border-top: 1px solid ${Solid.BORDER_GREY};
-  background-color: ${Solid.GREY};
 
   @media ${media.mobile} {
     min-width: 335px;
