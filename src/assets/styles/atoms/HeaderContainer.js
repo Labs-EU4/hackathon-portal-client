18fcfb3c8d556f5421a263b94a40c17096521e39
Karim B <mattiakarim@live.it>
2020-03-04 13:53:56 +0100

    Move both HeaderContainer and Heading into the assets atoms folder after refactoring code

---
+++ src/assets/styles/atoms/HeaderContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  width: 100%; height: 60px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export default HeaderContainer;
--- src/assets/styles/atoms/Heading.js
+++ src/assets/styles/atoms/Heading.js
@@ -1,9 +1,7 @@
-import styled from "styled-components";
-import { type, Solid } from "../index";
+import styled from 'styled-components';
 
 export const H1 = styled.h1`
-  font-family: ${type.ROBOTO};
-  color: ${Solid.BLACK};
+  color: ${props => props.theme.color.black.regular};
   font-size: 32px;
   font-weight: 700;
   white-space: nowrap;
@@ -12,18 +10,15 @@ export const H1 = styled.h1`
 `;
 
 export const H2 = styled.h2`
-  font-family: ${type.ROBOTO};
-  color: ${Solid.BLACK};
+  color: ${props => props.theme.color.black.regular};
   font-size: 26px;
   font-weight: 700;
   margin: 0 0 10px 0;
   padding: 0;
-  overflow: hidden;
 `;
 
 export const H3 = styled.h3`
-  font-family: ${type.ROBOTO};
-  color: ${Solid.BLACK};
+  color: ${props => props.theme.color.black.regular};
   font-size: 20px;
   font-weight: 500;
   margin: 0 0 10px 0;
@@ -31,10 +26,8 @@ export const H3 = styled.h3`
 `;
 
 export const H4 = styled.h4`
-  font-family: ${type.ROBOTO};
-  color: ${Solid.BLACK};
+  color: ${props => props.theme.color.black.regular};
   font-size: 16px;
   font-weight: 600;
-  margin: 10px 0;
-  padding: 0;
+  padding: 10px 0;
 `;
