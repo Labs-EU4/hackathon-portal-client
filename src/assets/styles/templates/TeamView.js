962f1af50dbd26fdb027b8355391e02ef4317a3f
Karim B <mattiakarim@live.it>
2020-03-05 00:11:04 +0100

    Change TeamView component logic and styles, moved styles to the assets templates folder

---
+++ src/assets/styles/templates/TeamView.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+import BodyContainer from "../atoms/BodyContainer";
+import { BoldSpan } from "../atoms/Span";
+
+export const TeamsContainer = styled(BodyContainer)`
+  background-color: white;
+  width: 50%;
+  height: 40%;
+  overflow-y: auto;
+  border: 1px solid lightgray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 25%;
+  border-radius: 6px;
+  padding: 20px;
+
+  p {
+    margin: 0;
+    padding: 3px;
+  }
+
+  button {
+    margin: 20px;
+  }
+`;
+
+export const FancyBoldSpan = styled(BoldSpan)`
+  border-bottom: 1px solid lightgray;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 100%;
+`;
+
+export const StyledLetterIcon = styled.div`
+  border: none; border-radius: 50%;
+`;
+
+export const NormalSpan = styled(BoldSpan)`
+  font-weight: normal;
+  padding: 5px;
+`;
