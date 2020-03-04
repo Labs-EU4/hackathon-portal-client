d439c833cca8dc0a7f855775219e7a4f6ad5e33e
Karim B <mattiakarim@live.it>
2020-03-05 00:35:32 +0100

    Refactor ResetPasswordConfirmation component

--- src/components/views/resetPassword/ResetPasswordConfirmation.jsx
+++ src/components/views/resetPassword/ResetPasswordConfirmation.jsx
@@ -1,38 +1,34 @@
 import React from "react";
-import BodyContainer from "../../atoms/BodyContainer";
-import HeroImage from "../../atoms/HeroImage";
-import Container from "../../atoms/Container";
-import { H1 } from "../../atoms/Heading";
-import { Paragraph } from "../../atoms/Paragraph";
-import { RowBody } from "../../atoms/RowBody";
-import WideBody from "../../atoms/WideBody";
-import { Header, Footer } from "../../organisms/index";
+
+import BodyContainer from "../../../assets/styles/atoms/BodyContainer";
+import Container from "../../../assets/styles/atoms/Container";
+import { H1 } from "../../../assets/styles/atoms/Heading";
+import { Paragraph } from "../../../assets/styles/atoms/Paragraph";
+import { RowBody } from "../../../assets/styles/atoms/RowBody";
+import WideBody from "../../../assets/styles/atoms/WideBody";
 import Button from "../../atoms/Button";
-import image from "../../../assets/Password-sent.png";
+// import HeroImage from "../../atoms/HeroImage";
+// import image from "../../../assets/images/Password-sent.png";
 
 function ResetPasswordConfirmation() {
   return (
-    <div>
-      <Header />
-      <WideBody>
-        <BodyContainer justify="center">
-          <HeroImage src={image} alt="Password Sent" />
-          <Container>
-            <H1>Check your inbox</H1>
-            <Paragraph>
-              Please check your email inbox for directions on how to reset your
-              password.
-            </Paragraph>
-            <RowBody>
-              <Button anchor size="wide" to="/login">
-                Return to Login page
-              </Button>
-            </RowBody>
-          </Container>
-        </BodyContainer>
-      </WideBody>
-      <Footer />
-    </div>
+    <WideBody>
+      <BodyContainer justify="center">
+        {/* <HeroImage src={image} alt="Password Sent" /> */}
+        <Container>
+          <H1>Check your inbox</H1>
+          <Paragraph>
+            Please check your email inbox for directions on how to reset your
+            password.
+          </Paragraph>
+          <RowBody>
+            <Button anchor size="wide" to="/login">
+              Return to Login page
+            </Button>
+          </RowBody>
+        </Container>
+      </BodyContainer>
+    </WideBody>
   );
 }
 
