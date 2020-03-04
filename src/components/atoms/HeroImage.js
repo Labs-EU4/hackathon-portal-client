ce668d7515189e5a64a56fad1833d56487b182ec
Karim B <mattiakarim@live.it>
2020-03-04 13:20:54 +0100

    Change format of the HeroImage component in the atoms folder

--- src/components/atoms/HeroImage.js
+++ src/components/atoms/HeroImage.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const HeroImage = ({ src, alt }) => <StyledHeroImage src={src} alt={alt} />;
   
-  export default HeroImage;
+export default HeroImage;
   
 const StyledHeroImage = styled.img`
   width: 100%;
