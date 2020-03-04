b898a581d545750d5612895c4ea3d53cd5d698c6
Karim B <mattiakarim@live.it>
2020-03-05 00:33:25 +0100

    Refactor code for the ResetPassword component

--- src/components/views/resetPassword/ResetPassword.jsx
+++ src/components/views/resetPassword/ResetPassword.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
-import Input from "../../atoms/Input";
-import WideBody from "../../atoms/WideBody";
-import BodyContainer from "../../atoms/BodyContainer";
-import { RowBody } from "../../atoms/RowBody";
-import { Footer } from "../../organisms/index";
-import Button from "../../atoms/Button";
-import { ErrorSpan } from "../../atoms/Span";
-import { forgotPassword } from "../../../store/user/actions";
-import Header from "../../organisms/Header";
 import { useHistory } from "react-router-dom";
+
+import BodyContainer from "../../../assets/styles/atoms/BodyContainer";
+import { RowBody } from "../../../assets/styles/atoms/RowBody";
+import { ErrorSpan } from "../../../assets/styles/atoms/Span";
+import Container from "../../../assets/styles/atoms/Container";
+import { H1 } from "../../../assets/styles/atoms/Heading";
+import { Paragraph } from "../../../assets/styles/atoms/Paragraph";
+import Button from "../../atoms/Button";
+import Input from "../../atoms/Input";
 import HeroImage from "../../atoms/HeroImage";
-import Container from "../../atoms/Container";
-import { H1 } from "../../atoms/Heading";
-import { Paragraph } from "../../atoms/Paragraph";
-import image from "../../../assets/Password.png";
+import image from "../../../assets/images/Password.png";
+import { forgotPassword } from "../../../store/user/actions";
 
 const ResetPassword = () => {
   const dispatch = useDispatch();
@@ -34,43 +32,37 @@ const ResetPassword = () => {
   });
 
   return (
-    <div>
-      <Header />
-      <WideBody>
-        <BodyContainer justify="center">
-          <HeroImage src={image} alt="Reset passowrd" />
-          <Container>
-            <H1>Reset the password</H1>
-            <Paragraph>
-              Enter your email address so we can reset your password and send a link to your inbox.
-            </Paragraph>
-            <Formik
-              initialValues={{ email: "" }}
-              onSubmit={handleSubmit}
-              validationSchema={schema}
-            >
-              {({ errors, touched }) => (
-                <Form>
-                  <Input display="wide" id="email" type="email" name="email" placeholder="Email address"/>
-                  {errors.name && touched.name ? (
-                    <div>{errors.name}</div>
-                  ) : null}
-                  <ErrorSpan>
-                    <ErrorMessage name="email" />
-                  </ErrorSpan>
-                  <RowBody>
-                    <Button size="wide" type="submit" color="blue">
-                      Reset Password
-                    </Button>
-                  </RowBody>
-                </Form>
-              )}
-            </Formik>
-          </Container>
-        </BodyContainer>
-      </WideBody>
-      <Footer />
-    </div>
+    <BodyContainer justify="center">
+      <HeroImage src={image} alt="Reset passowrd" />
+      <Container>
+        <H1>Reset the password</H1>
+        <Paragraph>
+          Enter your email address so we can reset your password and send a link to your inbox.
+        </Paragraph>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={handleSubmit}
+          validationSchema={schema}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Input display="wide" id="email" type="email" name="email" placeholder="Email address"/>
+              {errors.name && touched.name ? (
+                <div>{errors.name}</div>
+              ) : null}
+              <ErrorSpan>
+                <ErrorMessage name="email" />
+              </ErrorSpan>
+              <RowBody>
+                <Button size="wide" type="submit" color="blue">
+                  Reset Password
+                </Button>
+              </RowBody>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </BodyContainer>
   );
 };
 
