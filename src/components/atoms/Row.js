342b90015cf8bbc83a728a035401115fa44a78cd
Karim B <mattiakarim@live.it>
2020-03-04 14:10:16 +0100

    Chnage ProfileImg component styles and remove redundant styling for the both the ProfileImg and Row components

--- src/components/atoms/Row.js
+++ src/components/atoms/Row.js
@@ -2,7 +2,5 @@ import styled from 'styled-components';
 
 export const Row = styled.div`
   display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-  margin-right: 20px;
+  margin-top: 20px; margin-right: 20px;
 `;
