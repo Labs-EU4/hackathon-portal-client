789511d88f7b537447a5ac2ae5d9771ec10992fa
Karim B <mattiakarim@live.it>
2020-03-04 12:50:36 +0100

    Moved images into apposite image folder in assets

---
+++ src/assets/styles/variables/fonts.js
@@ -0,0 +1,14 @@
+export const type = {
+    ROBOTO: "'Roboto', sans-serif",
+    ROBOTO_MONO: "'Roboto Mono', monospace"
+  };
+  
+  // font-sizes
+  export const baseFontSize = "1.6rem";
+  export const smallFontSize = "1.4rem";
+  export const h1FontSize = "7.5rem";
+  export const h2FontSize = "5.5rem";
+  export const h3FontSize = "3.5rem";
+  export const h4FontSize = "2.5rem";
+  
+  export const button_text = "16px";
