cd6938702e8dcb43fd9d5a9d8acbcd93b78d6101
Karim B <mattiakarim@live.it>
2020-03-04 13:47:36 +0100

    Separate & move DropDown component styles to the assets folder

--- src/assets/styles/atoms/Container.js
+++ src/assets/styles/atoms/Container.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import { media, Solid } from '../index';
+import { media } from '../../assets/styles/variables/media';
 
 const Container = styled.div`
   max-width: 380px;
-  padding: 60px 40px;
-  border: 1px solid ${Solid.BORDER_GREY};
-  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
+  border: 1px solid ${props => props.theme.color.grey.border};
   border-radius: 6px;
+  padding: 60px 40px;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
 
   @media ${media.tablet} {
     width: 340px;
---
+++ src/components/atoms/DropDown.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { resetUser } from "../../store/user/actions";
+
+export const Dropdown = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleLogOut = () => {
+    dispatch(resetUser(history));
+  };
+  return (
+    <UL>
+      <li>
+        <Link to="/dashboard/profile/edit">Settings</Link>
+      </li>
+      <li>
+        <span onClick={handleLogOut}>LogOut</span>
+      </li>
+    </UL>
+  );
+};
