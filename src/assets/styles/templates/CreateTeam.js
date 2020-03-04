c125342fed08ee7040ce78cf34bffa450ee8270a
Karim B <mattiakarim@live.it>
2020-03-04 23:16:34 +0100

    Change CreateTeam component and move styles to the assets template folder

--- src/assets/styles/templates/CreateTeam.js
+++ src/assets/styles/templates/CreateTeam.js
@@ -1,4 +1,4 @@
-import BodyContainer from "../../atoms/BodyContainer";
+import BodyContainer from "../atoms/BodyContainer";
 import styled from "styled-components";
 
 export const BodyRow = styled(BodyContainer)`
@@ -21,7 +21,7 @@ export const Form = styled.form`
   background-color: white;
   width: 50%;
   height: 35vh;
-  border: 1px solid lightgray;
+  border: 1px solid ${props => props.theme.color.grey.border};
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -38,16 +38,11 @@ export const Form = styled.form`
     width: 50%;
     padding: 10px;
     margin: 10px;
-    border: 1px solid lightgray;
+    border: 1px solid ${props => props.theme.color.grey.border};
     border-radius: 6px;
   }
   button {
     width: 50%;
     margin: 10px;
   }
-`;
-
-export const Title = styled.span`
-    color: "#273F92";
-    background-color: "aliceblue"
 `;
--- src/components/templates/CreateTeam.jsx
+++ src/components/templates/CreateTeam.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { createTeamName } from "../../store/participantTeams/actions";
 import { Formik } from "formik";
+
 import {
   BodyRow,
   BodyColumn,
-  Form,
-  Title
-} from "../styles/templates/CreateTeamStyling";
-import { Footer } from "../organisms/index";
-import UserHeader from "../organisms/UserHeader";
-import { RowHead } from "../atoms/RowHead";
-import { H3 } from "../atoms/Heading";
+  Form
+} from '../../assets/styles/templates/CreateTeam';
+import { RowHead } from "../../assets/styles/atoms/RowHead";
+import { H3 } from "../../assets/styles/atoms/Heading";
 import Button from "../atoms/Button";
 import TeamView from "./TeamView";
+import WideBody from "../../assets/styles/atoms/WideBody";
+import { createTeamName } from "../../store/participantTeams/actions";
 import { useTeams } from "../../hooks";
-import WideBody from "../atoms/WideBody";
-import Nav from "../molecules/Nav";
 
 const CreateTeam = () => {
   const dispatch = useDispatch();
@@ -26,7 +23,7 @@ const CreateTeam = () => {
   const { userId } = useSelector(state => state.currentUser);
   const [teams, fetchTeams] = useTeams(id);
   const team = teams.find(t => t.team_lead === userId);
-debugger
+
   useEffect(() => {
     fetchTeams();
   }, [fetchTeams]);
@@ -38,53 +35,53 @@ debugger
     };
     dispatch(createTeamName(teamData, history));
   };
-debugger
+
   const { event_title } = useSelector(state =>
     state.events.data.find(event => event.id === Number(id))
   );
-debugger
+
   return (
-    <div>
-      <UserHeader />
-      <WideBody>
-        <Nav />
-        <BodyRow>
-          <RowHead>
-            <H3>Participant Teams</H3>
-          </RowHead>
-          <BodyColumn>
-            {!team ? (
-              <Formik
-                initialValues={{ team_name: "" }}
-                onSubmit={handleTeamSubmit}
-              >
-                {props => (
-                  <Form onSubmit={props.handleSubmit}>
-                    <h4>
-                      You are creating a team for <Title>{event_title}</Title>
-                    </h4>
-                    <label>Team Name</label>
-                    <input
-                      type="text"
-                      onChange={props.handleChange}
-                      onBlur={props.handleBlur}
-                      value={props.values.name}
-                      name="team_name"
-                    />
-                    <Button type="submit" color="green">
-                      Submit
-                    </Button>
-                  </Form>
-                )}
-              </Formik>
-            ) : (
-              <TeamView {...{ team }} />
+    <WideBody>
+    <BodyRow>
+      <RowHead>
+        <H3>Participant Teams</H3>
+      </RowHead>
+      <BodyColumn>
+        {!team ? (
+          <Formik
+            initialValues={{ team_name: "" }}
+            onSubmit={handleTeamSubmit}
+          >
+            {props => (
+              <Form onSubmit={props.handleSubmit}>
+                <h4>
+                  You are creating a team for{" "}
+                  <span
+                    style={{ color: "#273F92", backgroundColor: "aliceblue" }}
+                  >
+                    {event_title}
+                  </span>
+                </h4>
+                <label>Team Name</label>
+                <input
+                  type="text"
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                  value={props.values.name}
+                  name="team_name"
+                />
+                <Button type="submit" color="green">
+                  Submit
+                </Button>
+              </Form>
             )}
-          </BodyColumn>
-        </BodyRow>
-      </WideBody>
-      <Footer />
-    </div>
+          </Formik>
+        ) : (
+          <TeamView {...{ team }} />
+        )}
+      </BodyColumn>
+    </BodyRow>
+    </WideBody>
   );
 };
 
