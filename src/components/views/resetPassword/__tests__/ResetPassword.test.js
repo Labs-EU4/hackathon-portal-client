e669c11f2df5ec20cfcfbd2e620412ccc028ee14
AbdelI <homeworkoracle@protonmail.com>
2020-03-05 17:35:46 +0100

    Test for he text node for the <paragraph> enter email address, passing

--- src/components/views/resetPassword/__tests__/ResetPassword.test.js
+++ src/components/views/resetPassword/__tests__/ResetPassword.test.js
@@ -33,6 +33,13 @@ describe("Asserts that text nodes on ResetPassword.js,render properly", () => {
   it("The text node for the <H1> Reset the password, renders properly ", () => {
     expect(component.queryByText(/Reset the password/i)).toBeInTheDocument();
   });
+  it("The text node for the <paragraph> enter email address, renders properly ", () => {
+    expect(
+      component.queryByText(
+        /Enter your email address so we can reset your password and send a link to your inbox./i
+      )
+    ).toBeInTheDocument();
+  });
   //   it("The text node for the <h3> Participant Teams text node, renders properly", () => {
   //     expect(component.queryByText(/Participant Teams/i)).toBeInTheDocument();
   //   });
