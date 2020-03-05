7365194337ec728f163a1e8e5988b1ef67da9f6e
AbdelI <homeworkoracle@protonmail.com>
2020-03-05 11:46:32 +0100

    Test for EditHackathon rendering, passing

--- src/components/templates/__tests__/EditHackathon.test.js
+++ src/components/templates/__tests__/EditHackathon.test.js
@@ -1,20 +1,64 @@
-import React from 'react'
-import { Router, Route } from 'react-router';
-import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react'
-import EditHackathon from '../EditHackathon';
-const history = createMemoryHistory()
+import React from "react";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import "@testing-library/jest-dom/extend-expect";
+import EditHackathon from "../EditHackathon";
+import { initialState } from "../../../utils/mockData";
 
-const renderWithRouter = Component => render(
-  <Router history={history}>
-    <Route component={Component} />
-  </Router>
-)
+const history = createMemoryHistory();
 
-describe("It should render <EditHackathon/> template correctly", () => {
-  
-    it("renders the User EditHackathon templatet correctly", () => {
-      const template = () => renderWithRouter(EditHackathon)
-      expect(template).toMatchSnapshot();
-    });
+let component;
+let mockStore;
+let store;
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    id: 1
+  }),
+  useRouteMatch: () => ({ url: "/dashboard/event/:id/participant_submission" })
+}));
+
+beforeEach(() => {
+  mockStore = configureStore();
+  store = mockStore(initialState);
+  component = render(
+    <Router history={history}>
+      <Provider store={store}>
+        <EditHackathon/>
+      </Provider>
+    </Router>
+  );
+});
+
+describe("Shows all the text nodes on CreateTeam.js that are contained on the making an event form", () => {
+  it("CreateTeam.js component renders properly", () => {
+    expect(component).toMatchSnapshot();
   });
+  // it("The text node for the first character of the current user mail, renders properly on the menu", () => {
+  //   expect(component.queryByText(/8/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the <h3> Participant Teams text node, renders properly", () => {
+  //   expect(component.queryByText(/Participant Teams/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the <h4> You are creating a team for text node, renders properly", () => {
+  //   expect(
+  //     component.queryByText(/You are creating a team for/i)
+  //   ).toBeInTheDocument();
+  // });
+  // it("The text node for the team being created for the selected event 'EuroHack' <span>, renders properly", () => {
+  //   expect(component.queryByText(/EuroHack/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the Team Name <label> input, renders properly", () => {
+  //   expect(component.queryByText(/Team Name/i)).toBeInTheDocument();
+  // });
+  // it("The role for the button used to submit the form to create a team renders properly", () => {
+  //   expect(component.getByRole(/button/i)).toBeInTheDocument();
+  // });
+  // it("The text node for the submit button <button>, renders properly", () => {
+  //   expect(component.getByText(/submit/i)).toBeInTheDocument();
+  // });
+});
