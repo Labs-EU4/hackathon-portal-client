0797ef7f22c1340bc317d33bc00df3b081c740a5
AbdelI <homeworkoracle@protonmail.com>
2020-03-05 18:27:21 +0100

    Test for The text node for the  new password <paragraph>, passing

--- src/components/views/resetPassword/__tests__/NewPassword.test.js
+++ src/components/views/resetPassword/__tests__/NewPassword.test.js
@@ -33,11 +33,11 @@ describe("Asserts that text nodes on NewPassword.js,render properly", () => {
   it("The text node for the <H1> Change the password, renders properly ", () => {
     expect(component.queryByText(/Change the password/i)).toBeInTheDocument();
   });
-  //   it("The text node for the  email confirmation, renders properly ", () => {
-  //     expect(
-  //       component.queryByText(
-  //         "Please check your email inbox for directions on how to reset your password."
-  //       )
-  //     ).toBeInTheDocument();
-  //   });
+  it("The text node for the  new password <paragraph>, renders properly ", () => {
+    expect(
+      component.queryByText(
+        "Please enter new password and confirm it in the second input field."
+      )
+    ).toBeInTheDocument();
+  });
 });
