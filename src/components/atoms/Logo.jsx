63c8e3923bf701f241d5f85e5fd67a0253ce5d6b
AbdelI <homeworkoracle@protonmail.com>
2020-03-16 18:10:06 +0100

    Implemented the logo

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { media } from "../../assets/styles/variables/index"
+import image from "../../assets/images/Hackton-logo.png";
+
+const Logo = styled.img.attrs({
+  alt: "Hackton - Organise hackathons",
+  src: image
+})`
+  height: 43px;
+
+  @media ${media.tablet} {
+    height: 35px;
+  }
+
+  @media ${media.mobile} {
+    height: 30px;
+  }
+`;
+
+export default Logo;
