e481fc448efd79b727d83b33e898ccadfe422ebb
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-03-16 21:50:51 +0000

    Add MediaIcons

---
+++ src/assets/styles/atoms/SocialIconContainerStyling.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Social = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 0.5rem;
+  a {
+    color: ${props => props.theme.color.grey.regular};
+    padding: 0;
+    margin: 20px 15px 0 15px;
+    ${props => props.theme.fontSize.h1};
+    display: inline-block;
+    cursor: pointer;
+    i {
+      font-size: 5rem;
+      transition: transform 0.25s ease-in;
+      margin: 0 auto;
+    }
+    &:hover i {
+      color: ${props => props.theme.color.grey.regular};
+    }
+  }
+`;
+
+export const SocialMediaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 1rem;
+  margin-top: 3rem;
+  text-transform: uppercase;
+  color: ${props => props.theme.color.white.regular};
+  font-weight: 500;
+  line-height: 1.4rem;
+  .dividerContainer {
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    font-weight: 50;
+    font-size: 10px;
+    p{
+      width: 40%;
+    }
+    .divider {
+      padding: 1rem;
+      width: 47%;
+    }
+  }
+`;
--- src/components/molecules/SocialMedia.js
+++ src/components/molecules/SocialMedia.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SocialMediaContainer } from "../atoms/SocialIcon";
+import { SocialMediaContainer } from "../../assets/styles/atoms/SocialIconContainerStyling";
 import MediaIcons from "./MediaIcons";
 import { StrikedSpan } from "../../assets/styles/atoms/Span";
 
