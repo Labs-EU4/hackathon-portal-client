d980f778d988820c3f602c5a8d3527be0d47c82d
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-03-16 19:12:31 +0000

    Add checkbox

--- src/components/molecules/Checkbox.js
+++ src/components/molecules/Checkbox.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useField } from "formik";
 import { CheckGroup } from '../../assets/styles/atoms/CheckGroup';
 import { Label } from "../../assets/styles/atoms/Label";
-import { StyledCheckboxField } from "../../assets/styles/molecules/CheckboxStyling"
+import { StyledCheckboxFieldN } from "../../assets/styles/molecules/CheckboxStyling"
 
 function Checkbox({ label, ...props }) {
   const [field] = useField(props);
 
   return (
     <CheckGroup>
-      <StyledCheckboxField id={props.value} type="checkbox" {...field} {...props} />
+      <StyledCheckboxFieldN id={props.value} type="checkbox" {...field} {...props} />
       <Label htmlFor={props.value}>{label}</Label>
     </CheckGroup>
   );
