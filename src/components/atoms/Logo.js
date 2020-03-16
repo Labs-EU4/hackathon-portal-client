229a8b72975cb1ca714a87ea986668c98be7f174
AbdelI <homeworkoracle@protonmail.com>
2020-03-16 16:57:01 +0100

    added the new code for logo

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -1,20 +1,34 @@
-import styled from "styled-components";
-import { media } from "../../assets/styles/variables/index"
-import image from "../../assets/images/Hackton-logo.png";
+import React from "react";
+// import image from "./../../assets/images/Hackton-logo.png";
+import {
+  StyledLogo,
+  StyledAimIcon,
+  StyledLaptopIcon
+} from "../../assets/styles/atoms/Logo";
 
-const Logo = styled.img.attrs({
-  alt: "Hackton - Organise hackathons",
-  src: image
-})`
-  height: 43px;
-
-  @media ${media.tablet} {
-    height: 35px;
-  }
-
-  @media ${media.mobile} {
-    height: 30px;
-  }
-`;
+const Logo = ({ size }) => {
+  return (
+    <StyledLogo {...{ size }}>
+      <StyledAimIcon icon="crosshairs" />
+      <StyledLaptopIcon icon="laptop-code" />
+      <p>HackHunt</p>
+    </StyledLogo>
+  );
+};
 
 export default Logo;
+
+// const Logo = styled.img.attrs({
+//   alt: "Hackton - Organise hackathons",
+//   src: image
+// })`
+//   height: 43px;
+
+//   @media ${media.tablet} {
+//     height: 35px;
+//   }
+
+//   @media ${media.mobile} {
+//     height: 30px;
+//   }
+// `;
