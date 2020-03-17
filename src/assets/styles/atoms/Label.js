358d3fafce143176009797387f9af88304b5dd4b
AbdelI <homeworkoracle@protonmail.com>
2020-03-17 11:46:02 +0100

    Deleted the old Label elment from label.js

--- src/assets/styles/atoms/Label.js
+++ src/assets/styles/atoms/Label.js
@@ -1,17 +1,6 @@
 import styled from "styled-components";
 import { type } from "../variables/index";
 
-export const Label = styled.label`
-  font-family: ${type.ROBOTO};
-  font-weight: bold;
-  color: #696969;
-  margin: 0 0 10px;
-`;
-
-
-
-
-
 export const LabelN = styled.label`
   display: inline-block;
   font-weight: bold;
