bad250c2be43d6f09015a5a7618459a9a4d8c945
Karim B <mattiakarim@live.it>
2020-03-18 11:16:11 +0100

    Move styles from App component to AppStyles file

--- src/App.jsx
+++ src/App.jsx
@@ -137,38 +137,6 @@ function App() {
 
 export default App;
 
-const AppContainer = styled.main`
-  display: grid;
-  grid-template-columns: ${({ active }) =>
-    active ? "20px auto auto 60px" : "20px auto auto 250px"};
-  grid-template-rows: 60px auto auto 25px;
-  grid-template-areas:
-    " header header header aside"
-    "  gap    main   main  aside"
-    "  gap    main   main  aside"
-    " footer footer footer aside";
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-`;
-
-const RoutesContainer = styled.div`
-  ${props => props.theme.shadow.box};
-  position: relative;
-  grid-area: main;
-  background-color: ${props => props.theme.color.white.bg};
-  border-radius: 5px;
-  overflow: hidden;
-
-  &::-webkit-scrollbar {
-    width: 0px;
-    height: 0;
-  }
-`;
-
-
-
-
 
 // import React from "react";
 // import { Route, Switch, Redirect } from "react-router-dom";
