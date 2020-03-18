4f5e2aef0f80e7c86571693df3400f6c8b1e74a9
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-03-18 09:45:08 +0000

    Rafactor resetpassword views

--- src/components/views/resetPassword/ResetPasswordConfirmation.jsx
+++ src/components/views/resetPassword/ResetPasswordConfirmation.jsx
@@ -1,39 +1,36 @@
 import React from "react";
+
 import { BodyContainer } from "../../../assets/styles/atoms/BodyContainer";
-import HeroImage from "../../atoms/HeroImage";
 import { Container } from "../../../assets/styles/atoms/Container";
-import { H1 } from "../../../assets/styles/atoms/Heading";
+import { H1 } from "../../../assets/styles/atoms/HeadingStyling";
 import { Paragraph } from "../../../assets/styles/atoms/Paragraph";
 import { RowBody } from "../../../assets/styles/atoms/RowBody";
 import { WideBody } from "../../../assets/styles/atoms/WideBody";
-import { Header, Footer } from "../../organisms/index";
 import Button from "../../atoms/Button";
+import HeroImage from "../../atoms/HeroImage";
 import image from "../../../assets/images/Password-sent.png";
 
 function ResetPasswordConfirmation() {
   return (
-    <div>
-      <Header />
-      <WideBody>
-        <BodyContainer justify="center">
-          <HeroImage src={image} alt="Password Sent" />
-          <Container>
-            <H1>Check your inbox</H1>
-            <Paragraph>
-              Please check your email inbox for directions on how to reset your
-              password.
-            </Paragraph>
-            <RowBody>
-              <Button anchor size="wide" to="/login">
-                Return to Login page
-              </Button>
-            </RowBody>
-          </Container>
-        </BodyContainer>
-      </WideBody>
-      <Footer />
-    </div>
+    <WideBody>
+      <BodyContainer justify="center">
+        <HeroImage src={image} alt="Password Sent" />
+        <Container>
+          <H1>Check your inbox</H1>
+          <Paragraph>
+            Please check your email inbox for directions on how to reset your
+            password.
+          </Paragraph>
+          <RowBody>
+            <Button anchor size="wide" to="/login">
+              Return to Login page
+            </Button>
+          </RowBody>
+        </Container>
+      </BodyContainer>
+    </WideBody>
   );
 }
 
 export default ResetPasswordConfirmation;
+
