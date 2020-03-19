8eb6fbe57224a1d887b647c1fb8a3e60a066ab1a
Karim B <mattiakarim@live.it>
2020-03-19 17:39:27 +0100

    Complete SearchWidget destructuring

--- src/components/templates/widgets/SearchWidget.jsx
+++ src/components/templates/widgets/SearchWidget.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 
 import Button from "../../atoms/Button";
 import { AddTeamParticipantContainer } from "../../../assets/styles/templates/AddParticipantTeamsStyling";
--- src/components/templates/widgets/widgets.js
+++ src/components/templates/widgets/widgets.js
@@ -1,11 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
 import styled from 'styled-components';
-
-import Button from "../atoms/Button";
-import { RowBody } from "../../assets/styles/atoms/RowBodyStyling";
-import { useSearchUserByEmail } from "../../hooks";
-
 
 export const UserWidget = ({ user, select, ...otherProps }) => {
   const StyledWidget = styled.div`
