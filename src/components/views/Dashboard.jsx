d47b8975e5ab156a294efb6ea5a6468b0ae01dfe
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 11:41:44 +0100

    Removed old code for previous UI form Dashboard.jsx

--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import { EventOnboarding } from "../templates";
-// import { fetchAllEvents } from "../../store/events/actions";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-
-//   React.useEffect(() => {
-//     dispatch(fetchAllEvents());
-//   }, [dispatch]);
-
-//   return <EventOnboarding />;
-// };
-
-// export default Dashboard;
 
 import React from "react";
 import { useDispatch } from "react-redux";
