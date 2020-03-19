f9b84a78648342cbb9126039ddab7b747d390053
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 17:19:56 +0100

    Imported NavLink in TeamViewStyling

--- src/assets/styles/templates/TeamViewStyling.js
+++ src/assets/styles/templates/TeamViewStyling.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { BodyContainer } from "../atoms/BodyContainerStyling";
 import { BoldSpan } from "../atoms/Span";
+import { NavLink } from "react-router-dom";
 
 export const TeamsContainer = styled(BodyContainer)`
   background-color: white;
