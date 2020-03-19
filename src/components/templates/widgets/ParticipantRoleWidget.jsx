3161e5b8dcd37bf017f185a263758266c662ee1f
Karim B <mattiakarim@live.it>
2020-03-19 17:44:34 +0100

    Add the useHistory hook on the ParticipantRoleWidget component

--- src/components/templates/widgets/ParticipantRoleWidget.jsx
+++ src/components/templates/widgets/ParticipantRoleWidget.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Button from "../../atoms/Button";
 import { RowBody } from "../../../assets/styles/atoms/RowBodyStyling";
