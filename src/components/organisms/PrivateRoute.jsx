b0dc0ae10d073a489964e34b54aae7f3867b46f1
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 11:33:42 +0100

    Added the private route

--- src/components/organisms/PrivateRoute.jsx
+++ src/components/organisms/PrivateRoute.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ render: Component, ...rest }) => {
   const { token } = useSelector(state => state.currentUser);
   return (
     <Route
       {...rest}
       render={({ location, ...props }) =>
-        token ? <Component {...props} /> : <Redirect to={{ pathname: "/register", state: { from: location.pathname } }} />
+        token ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/register", state: { from: location.pathname } }}
+          />
+        )
       }
     />
   );
