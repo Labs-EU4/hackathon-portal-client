0e2995284dcd7810537585b61051c86d1c957ee5
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 16:39:35 +0100

    Imported new style for Teamview styling

--- src/assets/styles/templates/TeamViewStyling.js
+++ src/assets/styles/templates/TeamViewStyling.js
@@ -1,8 +1,9 @@
-import { BodyContainer, BodyContainerN } from "../atoms/BodyContainerStyling";
 import styled from "styled-components";
-import { BoldSpan, BoldSpanN } from "../atoms/Span";
 
-export const TeamsCont = styled(BodyContainer)`
+import BodyContainer from "../atoms/BodyContainer";
+import { BoldSpan } from "../atoms/Span";
+
+export const TeamsContainer = styled(BodyContainer)`
   background-color: white;
   width: 50%;
   height: 40%;
@@ -32,67 +33,12 @@ export const FancyBoldSpan = styled(BoldSpan)`
   width: 100%;
 `;
 
-export const NormalSpan = styled(BoldSpan)`
-  font-weight: normal;
-  padding: 5px;
-`;
-
-export const TeamMateDiv = styled.div`
-  border-bottom: 1px solid lightgray;
-  width: 100%;
-  padding-bottom: 10px;
-`;
-
-export const Img = styled.img`
-  width: 7%;
-  height: 7%;
-  margin-left: 1%;
-  object-fit: cover;
-`;
-
-export const ImgWithBorder = styled.img`
-  width: 7%;
-  height: 7%;
-  margin-left: 1%;
-  object-fit: cover;
-  border-radius: 5px;
-`;
-
-// --------------------------------- NEW // --------------------------------- //
-
-export const TeamsContainerN = styled(BodyContainerN)`
-  ${({ theme }) => theme.shadow.box};
-  background-color: ${({ theme }) => theme.color.white.regular};
-  width: 50%;
-  overflow-y: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-left: 25%;
-  border-radius: 6px;
-  padding: 20px;
-  p {
-    margin: 0;
-    padding: 3px;
-  }
-  button {
-    margin: 20px;
-  }
-`;
-
-export const FancyBoldSpanN = styled(BoldSpanN)`
-  border-bottom: 1px solid lightgray;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  width: 100%;
-`;
-
-export const StyledLetterIconN = styled.div`
+export const StyledLetterIcon = styled.div`
   border: none;
   border-radius: 50%;
 `;
 
-export const NormalSpanN = styled(BoldSpanN)`
+export const NormalSpan = styled(BoldSpan)`
   font-weight: normal;
   padding: 5px;
 `;
