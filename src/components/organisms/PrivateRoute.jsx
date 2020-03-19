73fcf3127ff64fc510a108fde1fe675d56f527b1
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 11:34:17 +0100

    Added a comment to explain what is happening in the private route

--- src/components/organisms/PrivateRoute.jsx
+++ src/components/organisms/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
-
+//changed component to render, to be able to pass down props
 const PrivateRoute = ({ render: Component, ...rest }) => {
   const { token } = useSelector(state => state.currentUser);
   return (
