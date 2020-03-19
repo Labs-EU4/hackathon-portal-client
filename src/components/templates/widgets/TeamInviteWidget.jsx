0414e23a96e6f9dde1112590daf0fc0a70cb9147
Karim B <mattiakarim@live.it>
2020-03-19 17:08:33 +0100

    Correct imports in the TeamInviteWidget file

--- src/components/templates/widgets/TeamInviteWidget.jsx
+++ src/components/templates/widgets/TeamInviteWidget.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from "../atoms/Button";
+import { RowBody } from "../../assets/styles/atoms/RowBodyStyling";
 import { StyledContainer } from "../../assets/styles/templates/AddParticipantTeamsStyling";
 
 export const TeamInviteWidget = props => {
