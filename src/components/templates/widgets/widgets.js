bd33c7c7d1c7883cb3ff96576b149ac8312601bc
Karim B <mattiakarim@live.it>
2020-03-19 17:36:23 +0100

    Add import in the SearchWidget component

--- src/components/templates/widgets/widgets.js
+++ src/components/templates/widgets/widgets.js
@@ -5,13 +5,6 @@ import styled from 'styled-components';
 import Button from "../atoms/Button";
 import { RowBody } from "../../assets/styles/atoms/RowBodyStyling";
 import { useSearchUserByEmail } from "../../hooks";
-import {
-  StyledContainer,
-  AddTeamParticipantContainer,
-  StyledWidget
-} from "../../assets/styles/templates/AddParticipantTeamsStyling";
-
-import isEmail from "validator/lib/isEmail";
 
 
 const UserWidget = ({ user, select, ...otherProps }) => {
@@ -23,46 +16,5 @@ const UserWidget = ({ user, select, ...otherProps }) => {
     <StyledWidget key={user.id} onClick={() => select(user)} {...otherProps}>
       {user.email}
     </StyledWidget>
-  );
-};
-
-export const SearchWidget = props => {
-  const { setSelectedUser, setNoneUser } = props;
-  const history = useHistory();
-  const [matches, searchString, setSearchString] = useSearchUserByEmail();
-  const validateEmail = email => {
-    return isEmail(email);
-  };
-  const redirect = (location = "/dashboard") => {
-    history.push(location);
-  };
-  const inputRef = useRef(null);
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  return (
-    <AddTeamParticipantContainer display="wide">
-      <input
-        type="text"
-        value={searchString}
-        onChange={e => {
-          setSearchString(e.target.value);
-        }}
-        placeholder="Search by email"
-        ref={inputRef}
-      />
-      {matches.map(user => (
-        <UserWidget key={user.id} user={user} select={setSelectedUser} />
-      ))}
-      {!matches && validateEmail(searchString)
-        ? setNoneUser(searchString)
-        : setNoneUser(null)}
-      <Button 
-        color="grey" 
-        onClick={() => redirect(history)}>
-        Back to dashboard
-      </Button>
-    </AddTeamParticipantContainer>
   );
 };
