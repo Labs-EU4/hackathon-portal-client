f434715a6ebc704b1a0ad901de233ddaef919932
Karim B <mattiakarim@live.it>
2020-03-19 10:17:48 +0100

    Destruture part of the code related to the judges from HackathonSingle into new create EventJudge file

--- src/components/organisms/EventJudges.jsx
+++ src/components/organisms/EventJudges.jsx
@@ -1,1 +1,43 @@
 import React from 'react';
+
+import 
+
+const EventJudges = () => {
+    return (
+        <JudgesContainer>
+            {team.length === 0 ? (
+            <Paragraph>
+                No Judges have been selected for this event
+            </Paragraph>
+            ) : (
+            team.map(member => (
+                <JudgeCard key={member.user_id}>
+                {member.image_url === null ? (
+                    <JudgeImg
+                    alt="team member profile pic"
+                    src={userImg}
+                    />
+                ) : (
+                    member.image_url.map((mem, index) => {
+                    let memberProfile;
+                    memberProfile = JSON.parse(mem);
+                    return (
+                        <JudgeImg
+                        key={index}
+                        alt="team member profile pic"
+                        src={memberProfile.avatar}
+                        />
+                    );
+                    })
+                )}
+                <JudgeInfo>
+                    <p>{member.fullname}</p>
+                </JudgeInfo>
+                </JudgeCard>
+            ))
+            )}
+        </JudgesContainer>
+    );
+};
+
+export default EventJudges;
