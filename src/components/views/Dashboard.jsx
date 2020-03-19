783874ce9a439ece4e6f887ead3c6c464c9a23f9
AbdelI <homeworkoracle@protonmail.com>
2020-03-19 10:51:36 +0100

    Importing new usereventsonboarding to dashboard

--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -1,27 +1,6 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { EventOnboarding } from "../templates";
-import { fetchAllEvents } from "../../store/events/actions";
-
-const Dashboard = () => {
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    dispatch(fetchAllEvents());
-  }, [dispatch]);
-
-  return <EventOnboarding />;
-};
-
-export default Dashboard;
-
 // import React from "react";
 // import { useDispatch } from "react-redux";
-
-// import PrivateRoute from "../organisms/PrivateRoute";
-// import { UserEventsDashboard } from "../templates";
 // import { EventOnboarding } from "../templates";
-// import HackathonSinglePage from "./HackathonSinglePage";
 // import { fetchAllEvents } from "../../store/events/actions";
 
 // const Dashboard = () => {
@@ -31,7 +10,28 @@ export default Dashboard;
 //     dispatch(fetchAllEvents());
 //   }, [dispatch]);
 
-//   return <UserEventsDashboard />;
+//   return <EventOnboarding />;
 // };
 
 // export default Dashboard;
+
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import PrivateRoute from "../organisms/PrivateRoute";
+import { UserEventsDashboard } from "../templates/UserEventsDashboard";
+import { EventOnboarding } from "../templates";
+import HackathonSinglePage from "./HackathonSinglePage";
+import { fetchAllEvents } from "../../store/events/actions";
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchAllEvents());
+  }, [dispatch]);
+
+  return <UserEventsDashboard />;
+};
+
+export default Dashboard;
