08e1d0db388a965f24095942b7315368d22701ef
Karim B <mattiakarim@live.it>
2020-03-20 10:24:14 +0100

    CardStyling fully rectified and now working correctly across all components

--- src/assets/styles/templates/HackathonProjectsStyling.js
+++ src/assets/styles/templates/HackathonProjectsStyling.js
@@ -1,28 +1,21 @@
 import styled from "styled-components";
-import { media } from "../variables/index";
-import { BodyContainer } from "../atoms/BodyContainerStyling";
+
+import { media } from "../variables/media";
+import { RowBody } from "../atoms/RowBodyStyling";
 import { CardWide } from "../atoms/CardStyling";
-import { Paragraph } from "../atoms/Paragraph";
-import { RowBody } from "../atoms/RowBody";
-import React from "react";
+import { Paragraph } from "../atoms/ParagraphStyling";
 
 export const StyledWideBody = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute; top: 0; left: 0;
+  width: 100%; height: 100%;
+  background-color: rgba(0, 0, 0, .4);
   z-index: 100;
 `;
 
 export const StyledCardWide = styled(CardWide)`
   ${props => props.theme.shadow.box};
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 70%;
-  max-width: 800px;
+  position: absolute; top: 50%; left: 50%;
+  width: 70%; max-width: 800px;
   transform: translate(-50%, -50%);
   z-index: 200;
 `;
@@ -39,10 +32,8 @@ export const StyledRowBody = styled(RowBody)`
 
 export const Team = styled.h3`
   border-bottom: 2px solid ${props => props.theme.color.grey.regular};
-  margin: 0 auto;
-  padding: 5px 10px;
-  font-size: 1.8rem;
-  font-weight: bold;
+  margin: 0 auto; padding: 5px 10px;
+  font-size: 1.8rem; font-weight: bold; 
   text-align: center;
 `;
 
@@ -57,35 +48,36 @@ export const SubmissionEntry = styled.div`
 `;
 
 export const SubmissionContent = styled.div`
-  ${props => props.theme.flex.custom("center", "center", "column")};
+  ${props => props.theme.flex.custom('center', 'center', 'column')};
   padding: 10px;
 `;
 
-export const StrongN = styled.strong`
+export const Strong = styled.strong`
   font-weight: bold;
 `;
 
 export const Description = styled(Paragraph)`
   width: 100%;
-  margin: none;
-  padding-top: 10px;
+  margin: none; padding-top: 10px;
   overflow: hidden;
   /* Ellipsis not working, fix this feature */
-  text-align: center;
-  text-overflow: ellipsis;
+  text-align: center; text-overflow: ellipsis;
+
   @media ${media.tablet} {
     max-width: 100%;
     margin: 0 0 20px 0;
     padding: 0 20px;
   }
+
   @media ${media.mobile} {
     padding: 0;
   }
 `;
 
 export const RatingGroup = styled.div`
-  ${props => props.theme.flex.custom("center", "center", "column")};
+  ${props => props.theme.flex.custom('center', 'center', 'column')};
   margin-bottom: 20px;
+
   /* @media ${media.tablet} {
     margin: 0 0 20px 0;
   } */
