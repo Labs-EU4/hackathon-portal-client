c453efb6fda302fdec3547bf7e2302161d71f84f
Karim B <mattiakarim@live.it>
2020-03-20 12:51:06 +0100

    Refactor code to latest one for the AddParticipantTeamsStyling file

--- src/assets/styles/templates/AddParticipantTeams.js
+++ src/assets/styles/templates/AddParticipantTeams.js
@@ -1,31 +1,28 @@
 import styled from "styled-components";
-import { BodyContainer, BodyContainerN } from "../atoms/BodyContainerStyling";
-import { type, Solid, media } from "../variables/index";
 
-import React from "react";
+import { media } from "../variables/media";
+import BodyContainer from "../atoms/BodyContainerStyling";
 
-export const BodyContainerColumn = styled(props => (
-  <BodyContainer {...props} />
-))`
+export const BodyContainerColumn = styled(BodyContainer)`
   flex-direction: column;
   justify-content: start;
 `;
 export const StyledContainer = styled.div`
-  display: block;
   position: relative;
+  display: block;
 `;
 
-export const Container = styled.div`
+export const AddTeamParticipantContainer = styled.div`
   input {
-    font-family: ${type.ROBOTO};
     font-size: 16px;
     font-weight: 500;
-    color: ${Solid.BLACK};
-    border: 1px solid ${Solid.BORDER_GREY};
+    color: ${props => props.theme.color.black.regular};
+    border: 1px solid ${props => props.theme.color.grey.border};
     border-radius: 6px;
     padding: 10px;
     margin: 0 20px 10px 0;
-    ${({ display }) => (display === "wide" ? `width: 100%;` : `width: 180px;`)}
+    ${({ display }) =>
+    display === "wide" ? `width: 100%;` : `width: 180px;`}
 
     &:focus {
       transition: all 0.5s;
@@ -40,43 +37,6 @@ export const Container = styled.div`
 `;
 
 export const StyledWidget = styled.div`
-  margin-bottom: 10px;
-  cursor: pointer;
-`;
-
-// --------------------------------- NEW // --------------------------------- //
-
-export const BodyContainerColumnN = styled(BodyContainerN)`
-  flex-direction: column;
-  justify-content: start;
-`;
-export const StyledContainerN = styled.div`
-  position: relative;
-  display: block;
-`;
-
-export const AddTeamParticipantContainerN = styled.div`
-  input {
-    font-size: 16px;
-    font-weight: 500;
-    color: ${props => props.theme.color.black.regular};
-    border: 1px solid ${props => props.theme.color.grey.border};
-    border-radius: 6px;
-    padding: 10px;
-    margin: 0 20px 10px 0;
-    ${({ display }) => (display === "wide" ? `width: 100%;` : `width: 180px;`)}
-    &:focus {
-      transition: all 0.5s;
-      box-shadow: 0 0 3px #ddd;
-    }
-  }
-  @media ${media.tablet} {
-    width: 100%;
-    margin-right: 0;
-  }
-`;
-
-export const StyledWidgetN = styled.div`
-  margin-bottom: 10px;
-  cursor: pointer;
+margin-bottom: 10px;
+cursor: pointer;
 `;
