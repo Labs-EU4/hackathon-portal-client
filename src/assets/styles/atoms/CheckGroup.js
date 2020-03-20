d40283e71b70c8197e8e5638bd25199a67ef9b09
AbdelI <homeworkoracle@protonmail.com>
2020-03-20 10:41:22 +0100

    Brought new styling in Checkgroup.js

--- src/assets/styles/atoms/CheckGroup.js
+++ src/assets/styles/atoms/CheckGroup.js
@@ -1,19 +1,11 @@
 import styled from "styled-components";
 
-export const CheckGroup = styled.div`
-  width: ${props => props.short ? "120px" : "180px"};
+const CheckGroup = styled.div`
+  width: ${props => (props.short ? "120px" : "180px")};
+
   label {
     font-weight: normal;
   }
 `;
 
-// --------------------------------- NEW // --------------------------------- //
-
-
-export const CheckGroupN = styled.div`
-  width: ${props => props.short ? "120px" : "180px"};
-  
-  label {
-    font-weight: normal;
-  }
-`;
+export default CheckGroup;
