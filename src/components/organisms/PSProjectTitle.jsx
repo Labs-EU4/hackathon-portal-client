8ae526f87280a367dd893945bff967c0b2b74aab
Karim B <mattiakarim@live.it>
2020-03-20 11:45:05 +0100

    Correct import for Paragraph in  PSProjectTitle component

--- src/components/organisms/PSProjectTitle.jsx
+++ src/components/organisms/PSProjectTitle.jsx
@@ -5,7 +5,7 @@ import { ErrorMessage } from "formik";
 import Input from "../atoms/Input";
 import { Label } from "../../assets/styles/atoms/LabelStyling";
 import { Column } from "../../assets/styles/atoms/ColumnStyling";
-import { ParagraphN } from "../../assets/styles/atoms/Paragraph";
+import { Paragraph } from "../../assets/styles/atoms/ParagraphStyling";
 
    
 const ProjectTitle = (props) => {
@@ -13,48 +13,48 @@ const ProjectTitle = (props) => {
   const currentEvent = props.currentEvent
 
     return (
-        <>
-              <RowBodyN>
-                <ParagraphN>
-                  You are making a submission for the{" "}
-                  <strong>{currentEvent.event_title}</strong>. Please ensure you
-                  have read the event guidelines and have gone through the
-                  grading rubrics for this event before you make your
-                  submission.
-                </ParagraphN>
-              </RowBodyN>
-              <RowBodyN>
-                <Column>
-                  <Label htmlFor="project_title">Project Title</Label>
-                  <Input
-                    type="text"
-                    id="project_title"
-                    name="project_title"
-                    display="wide"
-                  />
-                  <ErrorSpanN>
-                    <ErrorMessage name="project_title" component="div" />
-                  </ErrorSpanN>
-                </Column>
-                <Column>
-                  <Label htmlFor="participant_or_team_nam">
-                    Team/Participant name
-                  </Label>
-                  <Input
-                    type="text"
-                    name="participant_or_team_name"
-                    id="participant_or_team_name"
-                    display="wide"
-                  />
-                  <ErrorSpanN>
-                    <ErrorMessage
-                      name="participant_or_team_name"
-                      component="div"
-                    />
-                  </ErrorSpanN>
-                </Column>
-              </RowBodyN>
-              </>
+      <>
+        <RowBodyN>
+          <ParagraphN>
+            You are making a submission for the{" "}
+            <strong>{currentEvent.event_title}</strong>. Please ensure you
+            have read the event guidelines and have gone through the
+            grading rubrics for this event before you make your
+            submission.
+          </ParagraphN>
+        </RowBodyN>
+        <RowBodyN>
+          <Column>
+            <Label htmlFor="project_title">Project Title</Label>
+            <Input
+              type="text"
+              id="project_title"
+              name="project_title"
+              display="wide"
+            />
+            <ErrorSpanN>
+              <ErrorMessage name="project_title" component="div" />
+            </ErrorSpanN>
+          </Column>
+          <Column>
+            <Label htmlFor="participant_or_team_nam">
+              Team/Participant name
+            </Label>
+            <Input
+              type="text"
+              name="participant_or_team_name"
+              id="participant_or_team_name"
+              display="wide"
+            />
+            <ErrorSpanN>
+              <ErrorMessage
+                name="participant_or_team_name"
+                component="div"
+              />
+            </ErrorSpanN>
+          </Column>
+        </RowBodyN>
+      </>
     )}
 
     export default ProjectTitle;
