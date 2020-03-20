17c7c70a2844c4aa12b3e53fefb46be432b7c66f
AbdelI <homeworkoracle@protonmail.com>
2020-03-20 11:46:39 +0100

    BodyContainer fixed on CreateTeamStyling

--- src/assets/styles/templates/CreateTeamStyling.js
+++ src/assets/styles/templates/CreateTeamStyling.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
-import { BodyContainerN } from "../atoms/BodyContainerStyling";
-import { WideBody } from '../atoms/WideBodyStyling';
+import { BodyContainer } from "../atoms/BodyContainerStyling";
+import { WideBody } from "../atoms/WideBodyStyling";
 
 export const StyledWideBody = styled(WideBody)`
-  position: absolute; top: 0; left: 0;
-  width: 100%; height: 100%;
-  background-color: rgba(0, 0, 0, .4);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
   z-index: 100;
 `;
 
