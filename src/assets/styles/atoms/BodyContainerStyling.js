c8e7498bc185c75e5001c546c49242a221bb4544
AbdelI <homeworkoracle@protonmail.com>
2020-03-20 10:00:42 +0100

    Took out the old BodyContainer element from BodyContainerStyling

--- src/assets/styles/atoms/BodyContainerStyling.js
+++ src/assets/styles/atoms/BodyContainerStyling.js
@@ -1,23 +1,6 @@
 import styled from "styled-components";
 import { media } from "../variables/media";
 
-export const BodyContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: ${({ justify }) => (justify ? justify : "start")};
-  padding: 60px 45px;
-
-  @media ${media.tablet} {
-    flex-direction: column;
-    padding: 60px 20px;
-    & > img {
-      display: none;
-    }
-  }
-`;
-
 export const BodyContainerN = styled.div`
   width: 100%;
   height: 100%;
