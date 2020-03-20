35661f8b5efe56f5e83467bc52b6c2ff61c58cbb
Karim B <mattiakarim@live.it>
2020-03-20 11:34:14 +0100

    Correct import for Label in Checkbox component

--- src/components/molecules/Checkbox.jsx
+++ src/components/molecules/Checkbox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useField } from "formik";
 import { CheckGroupN } from '../../assets/styles/atoms/CheckGroup';
-import { LabelN } from "../../assets/styles/atoms/Label";
+import { Label } from "../../assets/styles/atoms/LabelStyling";
 import { StyledCheckboxFieldN } from "../../assets/styles/molecules/CheckboxStyling"
 
 function Checkbox({ label, ...props }) {
@@ -10,7 +10,7 @@ function Checkbox({ label, ...props }) {
   return (
     <CheckGroupN>
       <StyledCheckboxFieldN id={props.value} type="checkbox" {...field} {...props} />
-      <LabelN htmlFor={props.value}>{label}</LabelN>
+      <Label htmlFor={props.value}>{label}</Label>
     </CheckGroupN>
   );
 }
