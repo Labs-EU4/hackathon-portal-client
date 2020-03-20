963ff3aa3e40f7e3f0fcda5b96a5070c6bcd67e7
Karim B <mattiakarim@live.it>
2020-03-20 12:07:13 +0100

    Restructure code in PSProjectTitle

--- src/components/organisms/PSProjectTitle.jsx
+++ src/components/organisms/PSProjectTitle.jsx
@@ -12,49 +12,50 @@ const ProjectTitle = (props) => {
 
   const currentEvent = props.currentEvent
 
-    return (
-      <>
-        <RowBody>
-          <Paragraph>
-            You are making a submission for the{" "}
-            <strong>{currentEvent.event_title}</strong>. Please ensure you
-            have read the event guidelines and have gone through the
-            grading rubrics for this event before you make your
-            submission.
-          </Paragraph>
-        </RowBody>
-        <RowBody>
-          <Column>
-            <Label htmlFor="project_title">Project Title</Label>
-            <Input
-              type="text"
-              id="project_title"
-              name="project_title"
-              display="wide"
-            />
-            <ErrorSpanN>
-              <ErrorMessage name="project_title" component="div" />
-            </ErrorSpanN>
-          </Column>
-          <Column>
-            <Label htmlFor="participant_or_team_nam">
-              Team/Participant name
-            </Label>
-            <Input
-              type="text"
+  return (
+    <>
+      <RowBody>
+        <Paragraph>
+          You are making a submission for the{" "}
+          <strong>{currentEvent.event_title}</strong>. Please ensure you
+          have read the event guidelines and have gone through the
+          grading rubrics for this event before you make your
+          submission.
+        </Paragraph>
+      </RowBody>
+      <RowBody>
+        <Column>
+          <Label htmlFor="project_title">Project Title</Label>
+          <Input
+            type="text"
+            id="project_title"
+            name="project_title"
+            display="wide"
+          />
+          <ErrorSpanN>
+            <ErrorMessage name="project_title" component="div" />
+          </ErrorSpanN>
+        </Column>
+        <Column>
+          <Label htmlFor="participant_or_team_nam">
+            Team/Participant name
+          </Label>
+          <Input
+            type="text"
+            name="participant_or_team_name"
+            id="participant_or_team_name"
+            display="wide"
+          />
+          <ErrorSpanN>
+            <ErrorMessage
               name="participant_or_team_name"
-              id="participant_or_team_name"
-              display="wide"
+              component="div"
             />
-            <ErrorSpanN>
-              <ErrorMessage
-                name="participant_or_team_name"
-                component="div"
-              />
-            </ErrorSpanN>
-          </Column>
-        </RowBody>
-      </>
-    )}
+          </ErrorSpanN>
+        </Column>
+      </RowBody>
+    </>
+  );
+}
 
-    export default ProjectTitle;
+export default ProjectTitle;
