4e5ec5ac7cd71d6ed180b7ecd3437edbf30f0fb8
AbdelI <homeworkoracle@protonmail.com>
2020-03-20 10:13:15 +0100

    Fixed the import on ResetPasswordConfirmation

--- src/components/views/resetPassword/ResetPasswordConfirmation.jsx
+++ src/components/views/resetPassword/ResetPasswordConfirmation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BodyContainer } from "../../../assets/styles/atoms/BodyContainerStyling";
+import { BodyContainerN } from "../../../assets/styles/atoms/BodyContainerStyling";
 import Container from "../../../assets/styles/atoms/ContainerStyling";
 import { H1 } from "../../../assets/styles/atoms/HeadingStyling";
 import { Paragraph } from "../../../assets/styles/atoms/ParagraphStyling";
