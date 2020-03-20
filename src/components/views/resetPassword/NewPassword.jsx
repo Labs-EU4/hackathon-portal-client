9aa36ed7f35e878d0ee166198b7ddc09a8841268
AbdelI <homeworkoracle@protonmail.com>
2020-03-20 10:14:50 +0100

    Fixed the import on NewPassword

--- src/components/views/resetPassword/NewPassword.jsx
+++ src/components/views/resetPassword/NewPassword.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 import { WideBody } from "../../../assets/styles/atoms/WideBodyStyling";
-import { BodyContainer } from "../../../assets/styles/atoms/BodyContainerStyling";
+import { BodyContainerN } from "../../../assets/styles/atoms/BodyContainerStyling";
 import Container from "../../../assets/styles/atoms/ContainerStyling";
 import { H1 } from "../../../assets/styles/atoms/HeadingStyling";
 import { Paragraph } from "../../../assets/styles/atoms/ParagraphStyling";
