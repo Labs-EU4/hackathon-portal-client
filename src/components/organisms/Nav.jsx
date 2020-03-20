084a475f8c3b171728926885f5772ecf0967c5fd
Karim B <mattiakarim@live.it>
2020-03-20 12:48:47 +0100

    Rectify N prefix from Nav component

--- src/components/organisms/Nav.jsx
+++ src/components/organisms/Nav.jsx
@@ -51,16 +51,16 @@ const Nav = ({
   );
 
   return (
-    <StyledNavN active={isSideBarOpen}>
-      <UserContainerN onClick={() => setIsProfileOpen(!isProfileOpen)}>
-        <StyledProfileImageN
+    <StyledNav active={isSideBarOpen}>
+      <UserContainer onClick={() => setIsProfileOpen(!isProfileOpen)}>
+        <StyledProfileImage
           active={isEditProfileHovered}
           onMouseEnter={() => setIsEditProfileHovered(true)}
           onMouseLeave={() => setIsEditProfileHovered(false)}
         >
           {image_url !== null && token ? (
             <>
-              {isEditProfileHovered && <StyledEditIconN icon="user-edit" />}
+              {isEditProfileHovered && <StyledEditIcon icon="user-edit" />}
               <ProfileImg
                 image={image_url}
                 alt={username}
@@ -71,14 +71,14 @@ const Nav = ({
             <ProfileImg alt="defaultImg" {...{ isSideBarOpen }} />
           )}
           {!isSideBarOpen && (
-            <UserInfoContentN>
+            <UserInfoContent>
               <p>{fullname}</p>
               <p>{email}</p>
-            </UserInfoContentN>
+            </UserInfoContent>
           )}
-        </StyledProfileImageN>
-      </UserContainerN>
-      <StyledButtonN
+        </StyledProfileImage>
+      </UserContainer>
+      <StyledButton
         active={isSideBarOpen}
         exact
         link
@@ -88,11 +88,11 @@ const Nav = ({
         activeClassName="current"
       >
         Create{isSideBarOpen && <br />} Event
-      </StyledButtonN>
+      </StyledButton>
       {items.map(({ title, url, icon }, idx) => {
         return (
           <div key={idx} style={{ width: "100%", position: "relative" }}>
-            <StyledNavLinkN
+            <StyledNavLink
               active={isSideBarOpen}
               exact
               to={url}
@@ -101,17 +101,18 @@ const Nav = ({
             >
               <Icon {...{ icon }} />
               {!isSideBarOpen && <span>{title}</span>}
-            </StyledNavLinkN>
-            {isSideBarOpen && <LinkDetailsN>{title}</LinkDetailsN>}
+            </StyledNavLink>
+            {isSideBarOpen && <LinkDetails>{title}</LinkDetails>}
           </div>
         );
       })}
-      <StyledExpandIconN
+      <StyledExpandIcon
         icon="angle-double-down"
         active={isSideBarOpen}
         onClick={() => setIsSideBarOpen(!isSideBarOpen)}
       />
-    </StyledNavN>
+    </StyledNav>
   );
 };
+
 export default Nav;
