47b218d3ce061d35fd8213d5f13be41aac36db21
Karim B <mattiakarim@live.it>
2020-03-23 11:54:40 +0100

    Refactor code in SearchUserWidget component

--- src/components/templates/widgets/SearchUserWidget.jsx
+++ src/components/templates/widgets/SearchUserWidget.jsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useRef } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useRef } from 'react';
 
 import Button from "../../atoms/Button";
-import { UserWidget } from "./UserWidget"
-import { AddTeamParticipantContainer } from "../../../assets/styles/templates/AddParticipantTeamsStyling";
+import { RowBody } from "../../../assets/styles/atoms/RowBodyStyling";
+import { 
+    Container,
+    UsersList,
+    ChosenJudgesContainer,
+    ChosenJudgeImg,
+    StyledSearchIcon
+} from "../../../assets/styles/templates/AddTeammatesStyling";
+import { JudgeWidget } from './JudgeWidget';
 import isEmail from "validator/lib/isEmail";
 import { useSearchUserByEmail } from "../../../hooks";
 
-
 export const SearchUserWidget = props => {
-    const { setSelectedUser, setNoneUser } = props;
-    const history = useHistory();
+    const { 
+      selectedUsersHandler, 
+      selectedUserArr,
+      setNoneUser,
+      handleExit,
+      handleSubmit,
+    } = props;
     const [matches, searchString, setSearchString] = useSearchUserByEmail();
     const validateEmail = email => {
       return isEmail(email);
     };
-    const redirect = (location = "/dashboard") => {
-      history.push(location);
-    };
     const inputRef = useRef(null);
+  
     useEffect(() => {
       inputRef.current.focus();
     }, []);
   
     return (
-      <AddTeamParticipantContainer display="wide">
+      <Container display="wide">
         <input
           type="text"
           value={searchString}
@@ -34,17 +42,52 @@ export const SearchUserWidget = props => {
           placeholder="Search by email"
           ref={inputRef}
         />
-        {matches.map(user => (
-          <UserWidget key={user.id} user={user} select={setSelectedUser} />
-        ))}
-        {!matches && validateEmail(searchString)
-          ? setNoneUser(searchString)
-          : setNoneUser(null)}
-        <Button 
-          color="grey" 
-          onClick={() => redirect(history)}>
-          Back to dashboard
-        </Button>
-      </AddTeamParticipantContainer>
+        {
+          searchString.length > 0 
+            ? <StyledSearchIcon icon="times" onClick={() => setSearchString('')}/> 
+            : <StyledSearchIcon icon="search" />
+        }
+        <div style={{ display: "flex", flexDirection: "column"}}>
+          <UsersList>
+            {matches.map(user => (
+              // <UserWidget key={user.id} user={user} select={setSelectedUser} />
+              <JudgeWidget key={user.id} user={user} selected={selectedUsersHandler} />
+            ))}
+            {
+              !!matches && validateEmail(searchString) 
+                ? setNoneUser(searchString) 
+                : setNoneUser(null)
+            }
+          </UsersList>
+          <ChosenJudgesContainer>
+            {
+              selectedUserArr.current.length > 0 && (
+                selectedUserArr.current.map(user => {
+                  let memberProfile = JSON.parse(user.image_url);
+                  return user.image_url !== null ? (
+                    <ChosenJudgeImg src={memberProfile.avatar} alt={user.username}/>
+                  ) : (
+                    <ChosenJudgeImg 
+                      src="https://media.giphy.com/media/g0QET2Iejaa4EQ0eBV/giphy.gif" alt="default-img" 
+                    />
+                  )
+                }) 
+              )
+            }
+          </ChosenJudgesContainer>
+        </div>
+        <RowBody>
+          <Button 
+            color="grey" 
+            size="half" 
+            onClick={handleExit}
+          >Back to event</Button>
+          <Button 
+            color="green" 
+            size="half" 
+            onClick={handleSubmit}
+          >Add Teammate</Button>
+        </RowBody>
+      </Container>
     );
 };
