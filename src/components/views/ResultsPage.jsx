0456a0fdd6a5c876ad368ce871634ade1c624326
AbdelI <homeworkoracle@protonmail.com>
2020-03-23 19:28:37 +0100

    Adding array , to map over to add different markers for the many locations

--- src/components/views/ResultsPage.jsx
+++ src/components/views/ResultsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-
+// import image from "./anon.png";
 // Variables
 const GMAP = "AIzaSyBlSZRd1pc8t73WzRU7jDmnovZl287cxL8";
 const myLocation = {
@@ -12,8 +12,8 @@ const mapStyles = {
   width: "100%",
   height: "100%"
 };
-let currentLocation;
 
+let currentLocation;
 function ResultPage(props) {
   function showMap(position) {
     // Show a map centered at (position.coords.latitude, position.coords.longitude).
@@ -39,20 +39,47 @@ function ResultPage(props) {
     new window.google.maps.Map(googleMapRef.current, {
       zoom: 12,
       center: {
-        lat: currentLocation[0],
-        lng: currentLocation[1]
+        // lat: currentLocation[0],
+        // lng: currentLocation[1]
+        lat: 35.6762,
+        lng: 139.6503
       }
     });
 
-  let imageH = "https://picresize.com/images/rsz_1anonymous.png";
+  let imageH =
+    "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi2.png";
 
-  const createMarker = () =>
-    new window.google.maps.Marker({
-      position: { lat: currentLocation[0], lng: currentLocation[1] },
-      map: googleMap.current,
-      // label: "You",
-      icon: imageH
-    });
+  var beaches = [
+    // ["Coogee Beach", -33.923036, 151.259052, 5],
+    // ["vvvv", 35.6762, 139.6503,3],
+    // ["Cronulla Beach", -34.028249, 151.157507, 3],
+    // ["Bondi Beach", -33.890542, 151.274856, 4],
+    // // ["hey", currentLocation[0], currentLocation[1]],
+    ["Maroubra Beach", -33.950198, 151.259302, 1],
+    ["Manly Beach", -33.80010128657071, 151.28747820854187, 2]
+  ];
+
+  let createMarker;
+  
+  for (var i = 0; i < beaches.length; i++) {
+    var beach = beaches[i];
+    createMarker = () =>
+      new window.google.maps.Marker({
+        position: { lat: beach[1], lng: beach[2] },
+        map: googleMap.current,
+        icon: imageH,
+        label: "YOOOO"
+        // shape: shape,
+      });
+  }
+
+  // const createMarker = () =>
+  //   new window.google.maps.Marker({
+  //     position: { lat: currentLocation[0], lng: currentLocation[1] },
+  //     map: googleMap.current,
+  //     label: "You",
+  //     icon: imageH
+  //   });
 
   // useEffect Hook
   useEffect(() => {
@@ -66,7 +93,7 @@ function ResultPage(props) {
     });
   });
 
-  return currentLocation ? (
+  return true ? (
     <div id="google-map" ref={googleMapRef} style={mapStyles} />
   ) : (
     <p>Wait a moment while we find events in your area..</p>
