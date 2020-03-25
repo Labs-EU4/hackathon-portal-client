5b207fabd284ec6c8436d1c87c8753b83d805131
Karim B <mattiakarim@live.it>
2020-03-25 12:14:23 +0100

    Add new components to AboutPage

--- src/components/views/AboutPage.jsx
+++ src/components/views/AboutPage.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import { AboutContainer, AboutHeaderContainer, HeroContainer, HeroText, MainContent, SectionTitle, TeamContainer, StyledCard } from "../../assets/styles/views/AboutPage"
+import { 
+    AboutContainer, 
+    AboutHeaderContainer, 
+    HeroContainer, 
+    HeroText, 
+    MainContent, 
+    SectionTitle, 
+    TeamContainer, 
+    StyledCard,
+    Paragraph,
+    FeaturesContainer,
+    FeatureBox
+} from "../../assets/styles/views/AboutPage"
 
 import Logo from '../atoms/Logo';
 
@@ -65,13 +77,21 @@ const AboutPage = () => {
         <AboutContainer>
             <AboutHeaderContainer>
                 <HeroContainer>
-                    <Logo size="5rem"/>
+                    <Logo size="80%"/>
                     <HeroText>
                         Let's make your next hackathon a success!
                     </HeroText>
                 </HeroContainer>
             </AboutHeaderContainer>
             <MainContent>
+                <SectionTitle lineWidth="30%">Why HackHunt?<hr/></SectionTitle>
+                <Paragraph>We have great features that will make participating to hackathons fun again</Paragraph>
+                <FeaturesContainer>
+                    <FeatureBox></FeatureBox>
+                    <FeatureBox></FeatureBox>
+                    <FeatureBox></FeatureBox>
+                </FeaturesContainer>
+
                 <SectionTitle lineWidth="30%">Meet our team of superstars<hr/></SectionTitle>
                 <TeamContainer>
                     {
