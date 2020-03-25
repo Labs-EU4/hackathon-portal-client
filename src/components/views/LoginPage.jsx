a6515e20bc1685f30ad3c53947a2208211689119
AbdelI <homeworkoracle@protonmail.com>
2020-03-25 20:50:19 +0100

    Took out a console log

--- src/components/views/LoginPage.jsx
+++ src/components/views/LoginPage.jsx
@@ -4,9 +4,9 @@ import image from "../../assets/images/Login.png";
 import { UserOnboarding } from "../templates";
 
 const LoginPage = () => {
-  let currentLocation;
   function showMap(position) {
     // Show a map centered at (position.coords.latitude, position.coords.longitude).
+    let currentLocation;
 
     return (currentLocation = [
       position.coords.latitude,
@@ -14,7 +14,6 @@ const LoginPage = () => {
     ]);
   }
 
-  console.log("NAVIGATION", navigator.geolocation.getCurrentPosition(showMap));
   navigator.geolocation.getCurrentPosition(showMap);
 
   return (
