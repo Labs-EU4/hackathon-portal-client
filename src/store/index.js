811e606bec98b368e3946667a93208ad13ae8b50
AbdelI <homeworkoracle@protonmail.com>
2020-03-26 21:24:30 +0100

    took out logger from index

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
-import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import storage from "redux-persist/lib/storage";
 
