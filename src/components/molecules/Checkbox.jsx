ebbfe6ecf1c7b72e7642652fa9b36c9a1cf75327
AbdelI <homeworkoracle@protonmail.com>
2020-03-26 22:15:27 +0100

    fixed import on checkbox.jsx

--- src/components/molecules/Checkbox.jsx
+++ src/components/molecules/Checkbox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useField } from "formik";
 import { CheckGroup } from "../../assets/styles/atoms/CheckGroup";
 import { Label } from "../../assets/styles/atoms/LabelStyling";
-import { StyledCheckboxFieldN } from "../../assets/styles/molecules/CheckboxStyling";
+import { StyledCheckboxField } from "../../assets/styles/molecules/CheckboxStyling";
 
 function Checkbox({ label, ...props }) {
   const [field] = useField(props);
