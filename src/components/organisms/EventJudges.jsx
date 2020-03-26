b7fa1e47c26042bbf26d51fe1c89ecee01b4bc6d
AbdelI <homeworkoracle@protonmail.com>
2020-03-26 22:06:12 +0100

    reformatted eventjudges

--- src/components/organisms/EventJudges.jsx
+++ src/components/organisms/EventJudges.jsx
@@ -1,53 +1,45 @@
-import React from 'react';
+import React from "react";
 
 import {
-    JudgesContainer,
-    JudgeCard,
-    JudgeImg,
-    JudgeInfo
-} from '../../assets/styles/templates/HackathonSingleStyling';
+  JudgesContainer,
+  JudgeCard,
+  JudgeImg,
+  JudgeInfo
+} from "../../assets/styles/templates/HackathonSingleStyling";
 import { Paragraph } from "../../assets/styles/atoms/ParagraphStyling";
 import userImg from "../../assets/images/user_icon.svg";
 
-const EventJudges = ({
-    team,
-
-}) => {
-    return (
-        <JudgesContainer>
-            {team.length === 0 ? (
-            <Paragraph>
-                No Judges have been selected for this event
-            </Paragraph>
+const EventJudges = ({ team }) => {
+  return (
+    <JudgesContainer>
+      {team.length === 0 ? (
+        <Paragraph>No Judges have been selected for this event</Paragraph>
+      ) : (
+        team.map(member => (
+          <JudgeCard key={member.user_id}>
+            {member.image_url === null ? (
+              <JudgeImg alt="team member profile pic" src={userImg} />
             ) : (
-            team.map(member => (
-                <JudgeCard key={member.user_id}>
-                {member.image_url === null ? (
-                    <JudgeImg
+              member.image_url.map((mem, index) => {
+                let memberProfile;
+                memberProfile = JSON.parse(mem);
+                return (
+                  <JudgeImg
+                    key={index}
                     alt="team member profile pic"
-                    src={userImg}
-                    />
-                ) : (
-                    member.image_url.map((mem, index) => {
-                    let memberProfile;
-                    memberProfile = JSON.parse(mem);
-                    return (
-                        <JudgeImg
-                        key={index}
-                        alt="team member profile pic"
-                        src={memberProfile.avatar}
-                        />
-                    );
-                    })
-                )}
-                <JudgeInfo>
-                    <p>{member.fullname}</p>
-                </JudgeInfo>
-                </JudgeCard>
-            ))
+                    src={memberProfile.avatar}
+                  />
+                );
+              })
             )}
-        </JudgesContainer>
-    );
+            <JudgeInfo>
+              <p>{member.fullname}</p>
+            </JudgeInfo>
+          </JudgeCard>
+        ))
+      )}
+    </JudgesContainer>
+  );
 };
 
 export default EventJudges;
