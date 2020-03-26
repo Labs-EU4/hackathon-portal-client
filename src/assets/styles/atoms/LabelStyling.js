ecc134a76a29806e80ff1115557bdc6235d0773b
Karim B <mattiakarim@live.it>
2020-03-26 18:00:37 +0100

    Refactor styles to match new design in Label component

--- src/assets/styles/atoms/LabelStyling.js
+++ src/assets/styles/atoms/LabelStyling.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Label = styled.label`
   display: inline-block;
-  font-weight: bold;
+  font-weight: bold; font-size: 14px;
   color: ${props => props.theme.color.grey.light};
   margin-right: auto;
   padding: 5px;
